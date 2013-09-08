046da555548b4dc2f63cd603d3d53ac7225af1c8
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-07 23:01:25 -0700

    Multi-word commands
    A prime example of git weirdness here too.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <map>
 
 #include "Command.h"
 
@@ -23,6 +24,18 @@ void getInput(std::vector<std::string>* v)
     }
 }
 
+void splitWords(std::string str, std::vector<std::string>* v)
+{
+    v->clear();
+
+    std::stringstream sBuffer(str);
+    std::string word;
+    while(sBuffer >> word)
+    {
+        v->push_back(word);
+    }
+}
+
 void printVec(std::vector<std::string>* v)
 {
     for(std::vector<std::string>::iterator it = v->begin(); it != v->end(); ++ it)
@@ -31,71 +44,88 @@ void printVec(std::vector<std::string>* v)
     }
 }
 
-class CmdEat: public Command
+std::map<std::string, std::string> cmdByAlias;
+
+bool alistatify(std::vector<std::string>* words)
 {
-    public:
-        unsigned int execute(std::vector<std::string>* v)
+    if(words->size() == 0)
+    {
+        return false;
+    }
+
+    // loop through all commands
+    for(std::map<std::string, std::string>::iterator cait = cmdByAlias.begin(); cait != cmdByAlias.end(); ++ cait)
+    {
+        //
+        std::cout << "- Testing for " << cait->first << std::endl;
+
+        // get the words in its name
+        std::vector<std::string> cmdsWords;
+        splitWords(cait->first, &cmdsWords);
+
+        // check if we don't have enough input for this to be it
+        if(words->size() < cmdsWords.size())
         {
-            if(v->size() < 2)
+            // skip
+            continue;
+        }
+
+        //
+        bool itIs = true;
+
+        // loop through all command words
+        std::vector<std::string>::iterator cmdsWordIt = cmdsWords.begin();
+        std::vector<std::string>::iterator wordIt = words->begin();
+        while(cmdsWordIt != cmdsWords.end())
+        {
+            // compare it to the input
+            if(*cmdsWordIt != *wordIt)
             {
-                std::cout << "You ate nothing!" << std::endl;
-
-                return 0;
+                itIs = false;
+                break;
             }
-            std::cout << "You munched on a " << v->at(1) << std::endl;
 
-            return 100;
+            ++ cmdsWordIt;
+            ++ wordIt;
         }
-};
 
-class CmdErroneous: public Command
-{
-    public:
-        unsigned int execute(std::vector<std::string>* v)
+        //
+        if(itIs)
         {
-            std::cout << "This is an erroneous statement!" << std::endl;
+            // Do whatever
+            std::cout << cait->second << std::endl;
 
-            return 666;
+            // We did something!
+            break;
         }
-};
+    }
+}
 
 int main()
 {
-    //CommandFunction cf = new
-
-    // Commands
-    (new CmdErroneous())->setErroneous();
-    (new CmdEat())->addAlias(0, "eat");
+    cmdByAlias.insert(std::pair<std::string, std::string>("eat", "MUNCH!"));
+    cmdByAlias.insert(std::pair<std::string, std::string>("two words", "DOUBLE TROUBLE!"));
+    cmdByAlias.insert(std::pair<std::string, std::string>("take", "KLEPTOMANIA!"));
+    cmdByAlias.insert(std::pair<std::string, std::string>("take a dump", "PLOP!"));
 
     std::cout << "Fuzzy Computing Machine" << std::endl;
 
     bool running = true;
 
-    std::vector<std::string>* v = new std::vector<std::string>();
+    std::vector<std::string> v;
 
     while(running)
     {
         std::cout << "Enter something:" << std::endl;
         std::cout << "FCM:\\>";
-        getInput(v);
+        getInput(&v);
         std::cout << std::endl;
         std::cout << "You entered:" << std::endl;
-        printVec(v);
+        printVec(&v);
         std::cout << std::endl;
         std::cout << "Trying to recognize command..." << std::endl;
 
-        Command* theCmd = Command::getCommand(v->at(0));
-
-        if(theCmd == Command::erroneous())
-        {
-            std::cout << "Failed!" << std::endl;
-        }
-        else
-        {
-            std::cout << "Command recognized!" << std::endl;
-
-            theCmd->execute(v);
-        }
+        alistatify(&v);
 
         std::cout << std::endl;
     }
