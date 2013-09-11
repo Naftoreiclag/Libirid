5e7c4617741e4ebe23aec51db43377be17d3e42c
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-10 22:03:30 -0700

    Dictionary support for all three word types.

--- src/language/Dictionary.cpp
+++ src/language/Dictionary.cpp
@@ -6,8 +6,17 @@
 #include "WordNoun.h"
 #include "Grammar.h"
 
+// We will never instantiate you
+Dictionary::Dictionary() {}
+
+// =====
+// Nouns
+// =====
+
+// Vector
 std::vector<WordNoun*> Dictionary::registeredNouns;
 
+// Add
 NounId Dictionary::addNoun(WordNoun* newNoun)
 {
     registeredNouns.push_back(newNoun);
@@ -15,16 +24,68 @@ NounId Dictionary::addNoun(WordNoun* newNoun)
     return registeredNouns.size() - 1;
 }
 
-std::size_t Dictionary::numNouns()
-{
-    return registeredNouns.size();
-}
-
+// Get
 WordNoun* Dictionary::getNoun(NounId nounId)
 {
     return registeredNouns.at(nounId);
 }
 
-Dictionary::Dictionary()
+// Number of
+std::size_t Dictionary::numNouns()
 {
+    return registeredNouns.size();
+}
+
+// ========
+// Adjuncts
+// ========
+
+// Vector
+std::vector<WordAdjunct*> Dictionary::registeredAdjuncts;
+
+// Add
+AdjunctId Dictionary::addAdjunct(WordAdjunct* newAdjunct)
+{
+    registeredAdjuncts.push_back(newAdjunct);
+
+    return registeredAdjuncts.size() - 1;
+}
+
+// Get
+WordAdjunct* Dictionary::getAdjunct(AdjunctId adjunctId)
+{
+    return registeredAdjuncts.at(adjunctId);
+}
+
+// Number of
+std::size_t Dictionary::numAdjuncts()
+{
+    return registeredAdjuncts.size();
+}
+
+// =========
+// Modifiers
+// =========
+
+// Vector
+std::vector<WordModifier*> Dictionary::registeredModifiers;
+
+// Add
+ModifierId Dictionary::addModifier(WordModifier* newModifier)
+{
+    registeredModifiers.push_back(newModifier);
+
+    return registeredModifiers.size() - 1;
+}
+
+// Get
+WordModifier* Dictionary::getModifier(ModifierId modifierId)
+{
+    return registeredModifiers.at(modifierId);
+}
+
+// Number of
+std::size_t Dictionary::numModifiers()
+{
+    return registeredModifiers.size();
 }
--- src/language/Dictionary.h
+++ src/language/Dictionary.h
@@ -5,17 +5,42 @@
 #include <vector>
 
 #include "WordNoun.h"
+#include "WordAdjunct.h"
+#include "WordModifier.h"
 #include "Grammar.h"
+
+/* Pretty much just a dynamic lib for the different kinds
+ * of words.
+ */
 
 class Dictionary
 {
     public:
+        // Nouns
+        static NounId addNoun(WordNoun* newNoun);
         static WordNoun* getNoun(NounId nounId);
         static std::size_t numNouns();
-        static NounId addNoun(WordNoun* newNoun);
-    protected:
+
+        // Adjuncts
+        static AdjunctId addAdjunct(WordAdjunct* newAdjunct);
+        static WordAdjunct* getAdjunct(AdjunctId adjunctId);
+        static std::size_t numAdjuncts();
+
+        // Adjuncts
+        static ModifierId addModifier(WordModifier* newModifier);
+        static WordModifier* getModifier(ModifierId modifierId);
+        static std::size_t numModifiers();
     private:
+        // Nouns
         static std::vector<WordNoun*> registeredNouns;
+
+        // Adjuncts
+        static std::vector<WordAdjunct*> registeredAdjuncts;
+
+        // Modifier
+        static std::vector<WordModifier*> registeredModifiers;
+
+        // Private, because we'll never instantiate this.
         Dictionary();
 };
 
