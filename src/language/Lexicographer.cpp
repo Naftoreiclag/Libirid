375c889db90bf260db7d286c45337626a7aa2feb
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-11 20:02:47 -0700

    Re-added WordAdjunct
    I accidentally removed it somehow... This worries me.

--- src/language/Lexicographer.cpp
+++ src/language/Lexicographer.cpp
@@ -7,9 +7,12 @@
 
 void Lexicographer::graph()
 {
+    Dictionary::addNoun(new WordNoun("cupcake"));
     Dictionary::addNoun(new WordNoun("bunny"));
     Dictionary::addNoun(new WordNoun("cookie"));
     Dictionary::addNoun(new WordNoun("potato"));
+    Dictionary::addNoun(new WordNoun("sheep", "sheep"));
+    Dictionary::addNoun(new WordNoun("couch"));
 }
 
 Lexicographer::Lexicographer()
