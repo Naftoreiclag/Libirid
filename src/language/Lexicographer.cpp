822bde3ddc4b368ce9b25ec4d0a4e66c9200998c
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-11 21:39:35 -0700

    More dictionary entries
    Yay!

--- src/language/Lexicographer.cpp
+++ src/language/Lexicographer.cpp
@@ -9,19 +9,27 @@
 
 void Lexicographer::graph()
 {
+    // Na-Ooh-Nu-S
     Dictionary::addNoun(new WordNoun("cupcake"));
     Dictionary::addNoun(new WordNoun("bunny"));
+    Dictionary::addNoun(new WordNoun("mouse", "mice"));
     Dictionary::addNoun(new WordNoun("cookie"));
     Dictionary::addNoun(new WordNoun("potato"));
     Dictionary::addNoun(new WordNoun("sheep", "sheep"));
     Dictionary::addNoun(new WordNoun("couch"));
 
+    // Adjuncts
     Dictionary::addAdjunct(new WordAdjunct("above", gmr::above));
     Dictionary::addAdjunct(new WordAdjunct("below", gmr::below));
 
-    Dictionary::addArticle("a", gmr::definite, gmr::solo);
-    Dictionary::addArticle("some", gmr::definite, gmr::mas);
+    // Meaningless modifiers
+    Dictionary::addArticle("a", gmr::indefinite, gmr::solo);
+    Dictionary::addArticle("some", gmr::indefinite, gmr::mas);
     Dictionary::addArticle("the", gmr::definite, gmr::solo);
+    Dictionary::addArticle("this", gmr::definite, gmr::solo);
+    Dictionary::addArticle("that", gmr::definite, gmr::solo);
+    Dictionary::addArticle("these", gmr::definite, gmr::mas);
+    Dictionary::addArticle("those", gmr::definite, gmr::mas);
 }
 
 Lexicographer::Lexicographer()
