5d98f03c0bff74ad832422c6fad760758ae12158
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-16 17:58:24 -0700

    Fixed eating nothing

--- src/command/CommandEat.cpp
+++ src/command/CommandEat.cpp
@@ -9,16 +9,32 @@ bool CommandEat::execute(gmr::SentenceState* stnc)
     if(stnc->nounStates->size() < 1)
     {
         Sysout::println("You did not eat anything!");
+
+        return false;
     }
 
     if(stnc->nounStates->front()->plurality == gmr::singular)
     {
-        Sysout::println("You ate a delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        if(stnc->nounStates->front()->definity == gmr::definite)
+        {
+            Sysout::println("You ate the delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        }
+        else
+        {
+            Sysout::println("You ate a delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        }
     }
 
     if(stnc->nounStates->front()->plurality == gmr::plural)
     {
-        Sysout::println("You ate some delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        if(stnc->nounStates->front()->definity == gmr::definite)
+        {
+            Sysout::println("You ate the delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        }
+        else
+        {
+            Sysout::println("You ate some delicious " + stnc->nounStates->front()->getUnmodifiedName() + ".");
+        }
     }
 
     return true;
