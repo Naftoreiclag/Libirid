db21ec5db7570e9c615b1d7942b45f6ab4d7bb62
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-16 18:18:22 -0700

    Moved command frame generator to tools/
    Maybe I should call it something else. It's acronym is cfg, which sounds
    like ConFiG.

--- tools/command-frame-generator/src/main.cpp
+++ tools/command-frame-generator/src/main.cpp
@@ -7,15 +7,15 @@ int main()
 {
     std::cout << "Enter a name for the new command classes:" << std::endl;
     std::cout << " Example:" << std::endl;
-    std::cout << "    Entering Foo will make classes CommandFoo.cpp and CommandFoo.h in src/command/" << std::endl;
-    std::cout << "    Entering Bar will make classes CommandBar.cpp and CommandBar.h in src/command/" << std::endl;
+    std::cout << "  Entering Foo makes classes CommandFoo.cpp and CommandFoo.h in src/command/" << std::endl;
+    std::cout << "  Entering Bar makes classes CommandBar.cpp and CommandBar.h in src/command/" << std::endl;
     std::string name;
     std::cin >> name;
 
     std::string qut = "\"";
 
     std::ofstream seep;
-    std::string consp = "..\\src\\command\\Command" + name + ".cpp";
+    std::string consp = "..\\..\\..\\src\\command\\Command" + name + ".cpp";
     seep.open(consp.c_str());
     seep << "#include " + qut + "Command" + name + ".h" + qut + "\n";
     seep << "\n";
@@ -39,7 +39,7 @@ int main()
     std::transform(uppercaseName.begin(), uppercaseName.end(), uppercaseName.begin(), ::toupper);
 
     std::ofstream aych;
-    std::string conah = "..\\src\\command\\Command" + name + ".h";
+    std::string conah = "..\\..\\..\\src\\command\\Command" + name + ".h";
     aych.open(conah.c_str());
     aych << "#ifndef COMMAND" + uppercaseName + "_H\n";
     aych << "#define COMMAND" + uppercaseName + "_H\n";
