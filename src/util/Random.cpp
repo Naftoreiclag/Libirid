c9916971c73acf2e8373ca55ca038c7d63a72bd8
Naftoreiclag <naftoreiclag@gmail.com>
2013-09-22 09:47:55 -0700

    "Random" number generator
    Doesn't do a thing.

---
+++ src/util/Random.cpp
@@ -0,0 +1,6 @@
+#include "Random.h"
+
+int Random::randomInt(int a, int b)
+{
+    return 4;
+}
---
+++ src/util/Random.h
@@ -0,0 +1,14 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+class mt19937;
+
+class Random
+{
+    public:
+        static int randomInt(int a, int b);
+    protected:
+    private:
+};
+
+#endif // RANDOM_H
