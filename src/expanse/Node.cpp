14495d0f1e5b11513d282b21f749ccbd480b44a2
Naftoreiclag <naftoreiclag@gmail.com>
2013-11-27 23:02:21 -0800

    Node stuffs
    Process terminated with status 0 (0 minutes, 1 seconds)
    0 errors, 0 warnings (0 minutes, 1 seconds)
    ^
    Oh yeah baby!

--- src/expanse/Node.cpp
+++ src/expanse/Node.cpp
@@ -1,13 +1,92 @@
 #include "Node.h"
 
+#define NULL 0
+
 Node::Node(Node* parent)
-: parent(parent)
 {
+    // Tell parent to adopt me
+    setParent(parent);
 }
-
-
 
 Node::~Node()
 {
-    //dtor
 }
+
+Node* Node::getParent() { return parent; }
+void Node::setParent(Node* newParent)
+{
+    // If the new parent is nobody
+    if(newParent == NULL)
+    {
+        // If current parent is already nobody
+        if(parent == NULL)
+        {
+            // Do nothing
+        }
+
+        // If the current parent is somebody
+        else
+        {
+            // Tell current parent to forget me
+            parent->forgetChild(this);
+
+            // My parent is nobody
+            parent = NULL;
+        }
+    }
+
+    // If the new parent is somebody
+    else
+    {
+        // Tell new parent to adopt me
+        newParent->adoptChild(this);
+    }
+}
+
+Node* Node::getChild() { return child; }
+void Node::adoptChild(Node* newChild)
+{
+    // Tell the child about his brother
+    newChild->sibling = child;
+
+    // If child is not orphan
+    if(newChild->parent != NULL)
+    {
+        // Tell child's parent to disown child
+        newChild->parent->forgetChild(newChild);
+    }
+
+    // Tell child that his parent is me
+    newChild->parent = this;
+
+    // My child is him
+    child = newChild;
+}
+void Node::forgetChild(Node* childToDisown)
+{
+    // Iterate through all children
+    Node* previousNode = NULL;
+    for(Node* node = child; node != NULL; node = node->sibling)
+    {
+        // If this one is the one to disown
+        if(node == childToDisown)
+        {
+            // If nobody is before it (i.e. the first one)
+            if(previousNode == NULL)
+            {
+                // Make my child to be his sibling
+                child = node->getSibling();
+            }
+
+            // Any other spot but first
+            else
+            {
+                // The previous child's sibling is now the childToDisown's sibling
+                previousNode->setSibling(node->getSibling());
+            }
+        }
+    }
+}
+
+Node* Node::getSibling() { return sibling; }
+void Node::setSibling(Node* newSibling) { sibling = newSibling; }
--- src/expanse/Node.h
+++ src/expanse/Node.h
@@ -1,6 +1,9 @@
 #ifndef NODE_H
 #define NODE_H
 
+/* Nodes. THE COMMENTS ARE SUPER CREEPY!!!
+ *
+ */
 
 class Node
 {
@@ -9,16 +12,32 @@ class Node
         virtual ~Node();
 
         Node* getParent();
-        void setParent(Node* node);
 
-        Node* getSibling();
+        // Adopts a parent
+        // - New parent calls adoptChild(me);
+        void setParent(Node* newParent);
 
         Node* getChild();
-        Node* addChild();
+
+        // Adopts a child
+        // - New child knows who his siblings are
+        // - Old parent knows that he is no longer her child
+        // - I know that he is my new son
+        // - New child knows that I am his new parent
+        void adoptChild(Node* newChild);
+
+        // Forgets a child
+        // - I know that he is no longer my child
+        // - My children know that he is no longer their sibling.
+        // ! Disowned child does NOT know that he has been disowned, and still thinks that I am his parent!
+        void forgetChild(Node* childToDisown);
+
+        Node* getSibling();
+        void setSibling(Node* newSibling);
     private:
         Node* parent;
-        Node* sibling;
         Node* child;
+        Node* sibling;
 };
 
 #endif // NODE_H
