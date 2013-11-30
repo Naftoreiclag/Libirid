bdb7ae106308ce2f1d940f08c9e4be2882f5a6d1
Naftoreiclag <naftoreiclag@gmail.com>
2013-11-30 11:30:28 -0800

    Fixed license link
    I hate how doing this wipes all the cool messages on the individual
    files to all say "Fixed license link." Grrr

--- src/util/Sysout.cpp
+++ src/util/Sysout.cpp
@@ -1,6 +1,6 @@
 /* Copyright (c) 2013 https://github.com/Naftoreiclag/
  *
- * Distributed under the MIT License (referring to license referred to at http://opensource.org/licenses/mit-license.html)
+ * Distributed under the MIT License (http://opensource.org/licenses/mit-license.html)
  * See accompanying file LICENSE
  */
 
