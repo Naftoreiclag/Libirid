74989a82f6eab075de8a4e468eafd7f96d894246
Naftoreiclag <naftoreiclag@gmail.com>
2013-11-30 19:46:18 -0800

    Replaced Lua 5.1.5 with Lua 5.2.2

--- src/Libirid.cpp
+++ src/Libirid.cpp
@@ -8,8 +8,8 @@
 
 #include "Game.h"
 
-//#include "Lua_5-1-5/lua.h"
-//#include "LuaBridge/luabridge.h"
+#include "Lua_5-2-2/lua.h"
+#include "LuaBridge/luabridge.h"
 
 // Initialize
 void initialize()
