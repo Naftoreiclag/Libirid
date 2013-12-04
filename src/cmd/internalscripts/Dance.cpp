6ce2588f76edc220b23d34cff8e7154692536751
Naftoreiclag <naftoreiclag@gmail.com>
2013-12-03 22:03:38 -0800

    Made namespace in Dance.cpp logical
    I think my new namespace naming convention is to put them in namespaces
    with names derived from the folder names.

--- src/cmd/internalscripts/Dance.cpp
+++ src/cmd/internalscripts/Dance.cpp
@@ -10,13 +10,16 @@
 #include <vector>
 #include <string>
 
-namespace Dance
+namespace cmd
 {
-    bool execute(gmr::SentenceState* stnc, std::vector<std::string>* argumentWords, std::string alias)
+namespace scr
+{
+    bool dance(gmr::SentenceState* stnc, std::vector<std::string>* argumentWords, std::string alias)
     {
         Sysout::println("break it down!");
         Sysout::println();
 
         return true;
     }
+}
 }
