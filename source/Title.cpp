defb08888af84520dd89b9d963becd50a7c71d13
Naftoreiclag <naftoreiclag@gmail.com>
2013-12-20 12:06:40 -0800

    Fancier "Title Screen"

--- source/Title.cpp
+++ source/Title.cpp
@@ -10,15 +10,51 @@
 
 void Title::run()
 {
-    std::cout << std::endl;
-    std::cout << ".W........W......................W." << std::endl;
-    std::cout << ".W........W......................W." << std::endl;
-    std::cout << ".W.....W..W......W.........W.....W." << std::endl;
-    std::cout << ".W........W......................W." << std::endl;
-    std::cout << ".W...WWW..WWW..WWW..W.WW.WWW...WWW." << std::endl;
-    std::cout << ".W.....W..W..W...W..WW.....W..W..W." << std::endl;
-    std::cout << ".W.....W..W..W...W..W......W..W..W." << std::endl;
-    std::cout << ".W.....W..W..W...W..W......W..W..W." << std::endl;
-    std::cout << ".WWWW..W..WWW....W..W......W...WWW." << std::endl;
-    std::cout << std::endl;
+    //
+    std::cout << std::endl << std::endl;
+
+    // Solid char
+    char solid = 219;
+
+    // The logo
+    std::string logo[9] =
+    {
+        "100000000100000000000000000000001",
+        "100000000100000000000000000000001",
+        "100000100100000010000000001000001",
+        "100000000100000000000000000000001",
+        "100011100111001110010110111000111",
+        "100000100100100010011000001001001",
+        "100000100100100010010000001001001",
+        "100000100100100010010000001001001",
+        "111100100111000010010000001000111"
+    };
+
+    // For every line
+    for(int i = 0; i < 9; ++ i)
+    {
+        // Some space
+        std::cout << "                      ";
+
+        // For every char
+        for(int j = 0; j < 33; ++ j)
+        {
+            // If its one
+            if(logo[i].at(j) == '1')
+            {
+                // Print solid
+                std::cout << solid;
+            }
+
+            // If its zero
+            else
+            {
+                // Print blank
+                std::cout << ' ';
+            }
+        }
+
+        // Make space for next line
+        std::cout << std::endl;
+    }
 }
