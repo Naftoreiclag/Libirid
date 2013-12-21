88fb551d63969238cede51ae6a8b7d661c3b2f84
Naftoreiclag <naftoreiclag@gmail.com>
2013-12-21 14:17:49 -0800

    Added license header
    even to the files that don't have anything in them... yet!

--- source/Libirid_Options.cpp
+++ source/Libirid_Options.cpp
@@ -1,3 +1,9 @@
+/* Copyright (c) 2013 "Naftoreiclag" https://github.com/Naftoreiclag
+ *
+ * Distributed under the MIT License (http://opensource.org/licenses/mit-license.html)
+ * See accompanying file LICENSE
+ */
+
 #include "Libirid_Options.h"
 
 Libirid_Options::Libirid_Options()
